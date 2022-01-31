3939f5edb1230fd3946f7292901b9ae2f79e58a9
Adrish-dev-dl <adrishmitracrj@gmail.com>
2022-01-31 14:01:08 +0530

    Shell script to monitor memory parameters

---
+++ Memory_cpu_usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+filename="Mem_Cpu_$(date +%m-%y).txt"
+if [ ! -f "/dev/Mem_Cpu_$(date +%m-%y).txt" ] ; then
+	echo Date,Memory Usage,Disk Uage,Active Anonymous Blocks,Inactive Anonymous Blocks,Total Memmory,Free Memory,Available Memory>>$filename
+fi
+date | awk -vORS=, '{print $1" "$2" "$3" "$4" "$5" "$6}'>>$filename
+free -m | awk 'NR==2{printf "%s/%sMB (%.2f%),", $3,$2,$3*100/$2}'>>$filename
+iostat | awk 'NR==4{printf "%s%,",$1+$3}'>>$filename
+df -h /dev | awk 'NR==2{printf "%s,",$5}'>>$filename
+grep 'Active(anon)' /proc/meminfo | awk 'NR==1{printf "%s%s,", $2,$3}'>>$filename
+grep 'Inactive(anon)' /proc/meminfo | awk 'NR==1{printf "%s%s,", $2,$3}'>>$filename
+grep 'MemTotal' /proc/meminfo | awk 'NR==1{printf "%s%s,", $2,$3}'>>$filename
+grep 'MemFree' /proc/meminfo | awk 'NR==1{printf "%s%s,", $2,$3}'>>$filename
+grep 'MemAvailable' /proc/meminfo | awk 'NR==1{printf "%s%s\n", $2,$3}'>>$filename
+exit
